0b12cbdee66beb537bf9f13e3b27380830459556
Pradip Giri <pgiri@uplink.world>
2018-11-19 16:14:14 +0100

    Changes text on string.xml

--- app/src/main/java/de/blocklink/pgiri/pgd/Helper/UrlHelper.java
+++ app/src/main/java/de/blocklink/pgiri/pgd/Helper/UrlHelper.java
@@ -3,7 +3,7 @@ package de.blocklink.pgiri.pgd.Helper;
 public class UrlHelper {
     public static final String helpUrl = "https://shop.pigrid.com/help";
     public static final String shopUrl = "https://shop.pigrid.com/";
-    public static final String piSupplier = "https://uk.pi-supply.com/search?type=product&q=pigrid";
+    public static final String piSupplier = "https://stkbx.co/PiGrid";
 
     //service type
     public static final String serviceType = "urn:blocklink:pigrid:web:0";
